bb55ca83addb73552c129189a9b076fc1e60ee1d
himi <18361766+hizv@users.noreply.github.com>
2021-07-18 12:21:42 +0530

    JSX rewrite, useState, API fetch with useEffect

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,22 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Ka",
-      animal: "Dog",
-      breed: "Afghan Hound",
-    }),
-    React.createElement(Pet, {
-      name: "Ki",
-      animal: "Cat",
-      breed: "Kitty",
-    }),
-    React.createElement(Pet, { name: "Ko", animal: "Rabbit", breed: "Bunny" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));

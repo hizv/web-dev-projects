25f0161ef7b355443e9d7890508c5899ea2a658f
himi <18361766+hizv@users.noreply.github.com>
2021-07-27 22:16:59 +0530

    Populate details with API data

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,12 +1,40 @@
 import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 class Details extends Component {
-  constructor() {
-    super();
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
   }
   render() {
-    return <h2>Hello</h2>;
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {city}, {state}{" "}
+        </h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
   }
 }
 
-export default Details;
+export default withRouter(Details);
